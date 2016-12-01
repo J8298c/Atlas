f96a5bd37f3ce8346fe62d6250c92bb67fcf901a
j8298c <juliocmojica@gmail.com>
2016-11-30 23:16:00 -0500

    added google fonts and foursquare callback for explore

--- js/app.js
+++ js/app.js
@@ -31,6 +31,7 @@ function appViewModel() {
     //doesnt work with Model.markers.title or hard coded Model.locations.title
     self.createListings = ko.observableArray(Model.markers);
 
+
 }
 
 
@@ -39,6 +40,8 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: forestHills,
         zoom: 14,
+        mapTypeControl: false,
+        styles: [{ "featureType": "administrative", "elementType": "all", "stylers": [{ "visibility": "on" }, { "lightness": 33 }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2e5d4" }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#c5dac6" }] }, { "featureType": "poi.park", "elementType": "labels", "stylers": [{ "visibility": "on" }, { "lightness": 20 }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#c5c6c6" }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#e4d7c6" }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#fbfaf7" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "visibility": "on" }, { "color": "#acbcc9" }] }]
     });
     for (let i = 0; i < Model.locations.length; i++) {
         createMarker(Model.locations[i].location, Model.locations[i].title, Model.locations[i].content)
@@ -58,7 +61,7 @@ function initMap() {
         Model.markers.push(marker);
         let contentStr;
         for (let i = 0; i < Model.markers.length; i++) {
-            contentStr = '<div>' + Model.markers[i].title + '</div>' + '<div>' + Model.markers[i].content + '</div>';
+            contentStr = '<div class="marker-title">' + Model.markers[i].title + '</div>' + '<div class="marker-content">' + Model.markers[i].content + '</div>';
         }
         let bounds = new google.maps.LatLngBounds();
         //pushes new created marker into markers array in model
@@ -86,6 +89,28 @@ function initMap() {
             })
         }
     }
+
+    function getFourSquare() {
+        var client_id = "ZBCPIT5VPYBTT2KNLCWMIJU4BAL4ANCO4MPJHLSJBOYB3D32";
+        var client_secret = '2K5DPY4FG2DN2ZX4JSTWXHUN140LMR1JHGE04YCXOU0P303P';
+        //userless request url passing in search query from
+        //form input
+        var fourSquareUrl = "https://api.foursquare.com/v2/venues/explore" +
+            "?client_id=" + client_id +
+            "&client_secret=" + client_secret +
+            "&v=20130815" +
+            "&ll=" + 40.7181 + "," + -73.8448;
+        $.ajax({
+            url: fourSquareUrl,
+            dataType: 'json',
+        }).done(function(data) {
+            console.log(data);
+            // for (let i = 0; i < data.response.length; i++)
+            //     createMarker(data.response.venues[i].location, data.response.venues[i].name)
+        });
+        // initMap();
+    }
+    getFourSquare();
     ko.applyBindings(new appViewModel());
 
 }
